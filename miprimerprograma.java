2c4cec65d7e22d4eea009f8e9bf22f81ad18a127
Ing. Leonardo Lopez <53798794+ElectroComR4@users.noreply.github.com>
2020-05-25 18:24:08 -0300

    Ejercicio 1 Capitulo 1
    Pide el numero de la edad al usuario y lo muestra.

---
+++ miprimerprograma.java
@@ -0,0 +1,21 @@
+
+package miprimerprograma;
+
+import java.util.Scanner;
+
+public class miprimerprograma {
+
+ public static void main(String[] args) {
+     int edad;
+     
+     System.out.println("Ingrese su edad");
+     Scanner sc = new Scanner(System.in);
+     edad= sc.nextInt();
+     
+     System.out.println("Su edad es "+edad+" años");
+     
+     
+     
+    }
+    
+}
